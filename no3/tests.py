e2539190efc46ac229154bf440356ca9080b7044
Paul Bryant <paulwilliambryant@gmail.com>
2016-04-28 13:07:36 -0500

    First unit test and url mapping, dummy view

--- no3/tests.py
+++ no3/tests.py
@@ -1,8 +1,12 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from no3.views import login_page
+
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class LoginPageTest(TestCase):
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+    def test_root_url_resolves_to_login_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, login_page)
--- superno3/urls.py
+++ superno3/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from no3 import views
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.login_page, name="login_page"),
+    # url(r'^admin/', include(admin.site.urls)),
+
 ]
