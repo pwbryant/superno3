bdb5a4ed32e250b101ee8f0cd6859acf799620e3
Paul Bryant <paulwilliambryant@gmail.com>
2016-04-28 13:25:29 -0500

    Refactor login page view to use a template

--- no3/tests.py
+++ no3/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 from no3.views import login_page
 
 
@@ -10,3 +12,10 @@ class LoginPageTest(TestCase):
     def test_root_url_resolves_to_login_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, login_page)
+
+
+    def test_login_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = login_page(request)
+        expected_html = render_to_string('login.html')
+        self.assertEqual(response.content.decode(), expected_html)
